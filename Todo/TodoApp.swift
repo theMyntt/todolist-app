27efa8b09a69bd925ce3e5836f9cabfd45450945
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-18 18:45:21 -0300

    feat: initial commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Gabriel Araújo Lima on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
