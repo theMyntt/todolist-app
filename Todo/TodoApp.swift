58c9f6bd77a033c7f09181be312fea2d9c95d66e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-18 19:06:31 -0300

    chore: view inside a navigationview

--- Todo/TodoApp.swift
+++ Todo/TodoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TodoApp: App {
     var body: some Scene {
         WindowGroup {
-            ListView()
+            NavigationView {
+                ListView()
+            }
         }
     }
 }
